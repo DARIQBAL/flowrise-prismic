9e0d19732e697b1fe48de7b75316f7e3701e551b
dar iqbal <dar.iqbal.100@gmail.com>
2023-12-10 23:41:36 +0530

    feet: add gradient background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,8 @@ export default function RootLayout({
       <Header />
         {children}
         <Footer />
+        <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1]
+        inset-0 opacity-60" />
         </body>
     </html>
   );
